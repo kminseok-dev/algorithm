8592bcb4622096a2802d1196b85518a2b78b3adf
minseok <105479136+mkkim021@users.noreply.github.com>
2025-01-15 16:19:36 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+//vector<bool> isused;
+int arr[10];
+int n, m ;
+void func(int k){
+    if(k==m){
+        
+        for(int i=0; i<m; i++)cout<<arr[i]<<' ';
+        cout<<'\n';
+        
+        return;
+    }
+    for(int i=0;i<v.size();i++){
+        //if(!isused[i]){
+            arr[k] = v[i];
+            if(arr[k]<arr[k-1])continue;
+            //isused[i] = true;
+            func(k+1);
+            //isused[i] = false;
+        //}
+    }
+}
+
+int main(){
+    
+    cin >> n >> m;
+    v.resize(n);
+    //isused.resize(n, false);
+    for(int i=0;i<n;i++)cin>>v[i];
+    sort(v.begin(), v.end());
+    func(0);
+}
