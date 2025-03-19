ee0c4db4286ebec8bc0c97f63f13baadd187c3ee
minseok <105479136+mkkim021@users.noreply.github.com>
2025-03-19 20:45:50 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -11,10 +11,7 @@ int main(){
     cin>>n;
     dp[0] = 1; dp[2] = 3;
     for(int i=4;i<=n;i+=2){
-        dp[i] = 3 * dp[i-2];
-        for(int j=i-4;j>=0;j-=2){
-            dp[i] = dp[i] + 2*dp[j];
-        }
+        dp[i] = 4*dp[i-2]-dp[i-4];
     }
     cout<<dp[n];
     return 0;
