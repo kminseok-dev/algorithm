bc5221ca655657cd26db70697ab9b0865cbaed3c
minseok <105479136+mkkim021@users.noreply.github.com>
2025-03-20 18:29:55 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int dp[1001][1001];
+int main(){
+    int ans=0;
+    string s1,s2;
+    cin>>s1>>s2;
+    
+    for(int i=1;i<=s1.size();i++){
+        for(int j=1;j<=s2.size();j++){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j] = dp[i-1][j-1]+1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+            ans = max(ans,dp[i][j]);
+        }
+    }
+    cout<<ans;
+}
