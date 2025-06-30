5b0d6e201a589b9db9a4bc3839e23a48037afa64
minseok <105479136+mkkim021@users.noreply.github.com>
2025-06-30 19:02:25 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,60 +1,31 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-
-int calculate(string expr){
-    string num = "";
-    int sum = 0;
-    for(char c:expr){
-        if(c=='+'){
-            sum += stoi(num);
-            num = "";
-        }
-        else{
-            num+=c;
-        }
-    }
-    sum+=stoi(num);
-    
-    return sum;
-}
-int main(){
+ 
+int main() {
     string input;
-    string temp;
-    getline(cin,input);
-    
-    vector<string> a;
-    
-    int idx = 0;
-    for(char c:input){
-        if(c=='-'){
-            
-            a.push_back(temp);
-            // op.push_back(c);
-            temp = "";
+    cin >> input;
+ 
+    int result = 0;
+    string num;
+    bool minus = false;
+ 
+    for (int i = 0; i <= input.size(); i++) {
+        if (input[i] == '-' || input[i] == '+' || i==input.size()) {
+            if (minus) {
+                result -= stoi(num);
+                num = "";
+            }
+            else {
+                result += stoi(num);
+                num = "";
+            }
+            if (input[i] == '-') {
+                minus = true;
+            }    
         }
-        
-        else{
-            temp += c;
+        else {
+            num += input[i];
         }
     }
-    a.push_back(temp);
-    // for(auto s:a){
-    //     cout<<s<<'\n';
-    // }
-    // cout<<'\n';
-    vector<int> result;
-    for(auto s:a){
-        result.push_back(calculate(s));
-    }
-    // for(int i=0;i<result.size();i++){
-    //     cout << result[i]<<'\n';
-    // }
-    int ans = result[0];
-    
-    for(int i=1;i<result.size();i++){
-        ans -= result[i];
-    }    
-    cout << ans;
+    cout << result;
 }
