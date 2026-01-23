a92306b3e9872df2c7646f1c519771abaf5ea88c
kimminseok <105479136+kminseok-dev@users.noreply.github.com>
2026-01-24 03:41:23 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,62 +1,56 @@
 #include<bits/stdc++.h>
-#include<algorithm>
 
 #define X first
 #define Y second 
 
 using namespace std;
 
+typedef pair<int,pair<int,int>> Node;
+typedef priority_queue<Node,vector<Node>,greater<Node>> PQ;
+
 int n,cnt,weight = 2;
 int sea[22][22];
-
-priority_queue<pair<int,pair<int,int>>,
-            vector<pair<int,pair<int,int>>>,
-            greater<pair<int,pair<int,int>>>> pq;
-
-
-void bfs(pair<int,int> pos){
-
-    int dist[22][22];    
+int dist[22][22];
+PQ bfs(pair<int,int> pos){
+    
     int dx[4] = {-1,0,1,0};
     int dy[4] = {0,-1,0,1};
 
-    fill(&dist[0][0],&dist[21][22],-1);
+    memset(dist,-1,sizeof(dist));
 
-    pq = priority_queue<pair<int,pair<int,int>>,
-                vector<pair<int,pair<int,int>>>,
-                    greater<pair<int,pair<int,int>>>>();
-
+    PQ pq;
     queue<pair<int,int>> q;    
     q.push({pos.X,pos.Y});
     dist[pos.X][pos.Y] = 0;
     int maxDist = 2e9;
 
     while(!q.empty()){
-        auto cur = q.front(); q.pop();
+        auto cur = q.front();
+        q.pop();
         if(maxDist <= dist[cur.X][cur.Y])continue;
         for(int i=0;i<4;i++){
             int nx = cur.X + dx[i];
             int ny = cur.Y + dy[i];
+            
             if(nx<1||ny<1||nx>n||ny>n)continue;
             if(sea[nx][ny]>weight)continue;
-            if(dist[nx][ny]==-1&&sea[nx][ny]<weight&&sea[nx][ny]>0){
-                dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-                maxDist = dist[nx][ny];
-                pq.push({dist[nx][ny],{nx,ny}});
-                q.push({nx,ny});
+            if(dist[nx][ny] != -1)continue;
+
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+            
+            if(sea[nx][ny]<weight&&sea[nx][ny]>0){
+                if(maxDist == 2e9){
+                    maxDist = dist[nx][ny];
+                }
+                pq.push({dist[nx][ny], {nx, ny}});
             }
-            else if(dist[nx][ny]==-1&&sea[nx][ny]<=weight){
-                dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-                q.push({nx,ny});
-            }
+            q.push({nx, ny});
         }
     }
-
-
-       
+    return pq;
 }
+
 int main(){
-    
     pair<int,int> pos;
     cin >> n;
 
@@ -68,30 +62,26 @@ int main(){
             }
         }
     }
-    int ans = 0;
 
+    int ans = 0;
+    
     while(1){
-        bfs(pos);
+        PQ pq = bfs(pos);
         if(pq.empty())break;
+
         cnt++;
         if(cnt==weight){
             weight++;
             cnt = 0;
         }
+
         sea[pos.X][pos.Y] = 0;
         ans += pq.top().X;
-        pos = {pq.top().Y.X,pq.top().Y.Y};
+        pos = pq.top().Y;
         sea[pos.X][pos.Y] = 9;
-        
-
     }
     
     cout << ans;
 
-    
-    
-
-    
-    
-    
+    return 0;
 }
