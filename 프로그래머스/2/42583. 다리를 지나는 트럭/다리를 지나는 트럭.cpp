06b9e8545db60f3ce20f633f9624ced36f96a63d
김민석 <105479136+kminseok-dev@users.noreply.github.com>
2026-07-22 02:23:31 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.62 ms, Memory: 4.83 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int cnt = 1;
+    int num = 0;
+    int totalWeights = truck_weights[num];
+    queue<int>q;
+    q.push(num);
+    while(!q.empty()){
+        cnt++;
+        if(cnt>bridge_length&&q.front()!=-1)totalWeights-=truck_weights[q.front()];
+        if(cnt>bridge_length)q.pop();
+        if(num<truck_weights.size()-1){
+            if(totalWeights+truck_weights[num+1]<=weight){
+                num++;
+                q.push(num);
+                totalWeights+=truck_weights[num];  
+                cout << cnt << " " << num << " " << totalWeights << '\n';
+                continue;
+            }
+            q.push(-1);
+        }
+        
+        
+    }
+    return cnt;
+}
